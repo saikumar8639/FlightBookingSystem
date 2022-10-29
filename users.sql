2ab48fd879aca641aee1272b8e79b6bcfe082ecf
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2022-10-29 14:38:00 +0530

    first commit

--- users.sql
+++ users.sql
@@ -41,7 +41,26 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`uid`, `username`, `age`, `email`, `password`) VALUES
 (1, 'deep', 21, 'deepd12@gmail.com', '12345678');
+insert into users (uid,username,age,email,password) values(123,"SAI",18,"xyz@gmail.com","154jhs"),
+				(1153,"sjdkhd",65,"sdjkj@gmail.com","jajsdj145"),
+				(16562,"Ssdhf",15,"xzsjhd@hotmail.com","154j54hs"),
+				(9823,"Sdjfcnjd",26,"ddjss@gmail.com","ndhdu38"),
+				(126563,"dhjhfds",45,"xyz1234@gmail.com","154jhs"),
+				(1125263,"sjdkushhd",24,"sdjkj566@gmail.com","jajsdj145"),
+				(1265662,"Ssdhjdhf",46,"xzsjhd56@hotmail.com","4545727bs"),
+				(9826563,"Sdjhshfcnjd",29,"ddjss6595s3@gmail.com","6599344"),
+				(3998,"scffgg",69,"xyz@gmail.com","126jhs"),
+				(0053,"sjded",23,"sdjkddjj@gmail.com","jajsdj1455"),
+				(665922,"Sqhf",64,"xzsjhdfd@hotmail.com","158j54hs"),
+				(9823154,"Sgjfcnjd",52,"ddfgejss@gmail.com","ndh858"),
+				(5486,"dhjhghds",51,"xyz1234s@gmail.com","15spjhs"),
+				(18962,"sjdkfggshhd",82,"sdjsfkj566@gmail.com","jajs55145"),
+				(898123,"Ssdhhfhf",61,"xzsjhd5fg6@hotmail.com","4p5q77bs"),
+				(98231,"Sdjhsfkfcnjd",100,"ddjss6gc595s3@gmail.com","6r9rt34")
 
+
+
+;
 --
 -- Indexes for dumped tables
 --
